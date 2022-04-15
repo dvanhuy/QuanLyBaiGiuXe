d2c259145d8b6526c2fb576e8eb7906728d95e29
Duy <leducduydn.7122111@gmail.com>
2022-04-15 14:01:13 +0700

    define

---
+++ src/define/Xe.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package define;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Xe {
+    private String idXe, bienSo, timeVao, timeRa, trangThai, loaiXe;
+    private float tienDaThu;
+
+    public String getIdXe() {
+        return idXe;
+    }
+
+    public void setIdXe(String idXe) {
+        this.idXe = idXe;
+    }
+
+    public String getBienSo() {
+        return bienSo;
+    }
+
+    public void setBienSo(String bienSo) {
+        this.bienSo = bienSo;
+    }
+
+    public String getTimeVao() {
+        return timeVao;
+    }
+
+    public void setTimeVao(String timeVao) {
+        this.timeVao = timeVao;
+    }
+
+    public String getTimeRa() {
+        return timeRa;
+    }
+
+    public void setTimeRa(String timeRa) {
+        this.timeRa = timeRa;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public String getLoaiXe() {
+        return loaiXe;
+    }
+
+    public void setLoaiXe(String loaiXe) {
+        this.loaiXe = loaiXe;
+    }
+
+    public float getTienDaThu() {
+        return tienDaThu;
+    }
+
+    public void setTienDaThu(float tienDaThu) {
+        this.tienDaThu = tienDaThu;
+    }
+    
+}
---
+++ src/define/users.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package define;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class users {
+    private String idNV, tenNV, tenTK, matKhau, gioiTinh, queQuan, CMND, chucVu;
+
+    public String getIdNV() {
+        return idNV;
+    }
+
+    public void setIdNV(String idNV) {
+        this.idNV = idNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getTenTK() {
+        return tenTK;
+    }
+
+    public void setTenTK(String tenTK) {
+        this.tenTK = tenTK;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+
+    public String getCMND() {
+        return CMND;
+    }
+
+    public void setCMND(String CMND) {
+        this.CMND = CMND;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+    
+    
+}
