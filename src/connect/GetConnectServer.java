73b0ff0b512f730e9f3f895c27e5310d214092a2
unknown <huydinhvan13@gmail.com>
2022-04-17 13:34:52 +0700

    getconnect

---
+++ src/connect/GetConnectServer.java
@@ -0,0 +1,40 @@
+
+package connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetConnectServer {
+    public static Connection Connectionn(String may,String userName,String password){
+        String url="jdbc:sqlserver://"+may+"\\SQLEXPRESS:1433;databaseName=BaiGiuXe";
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            return DriverManager.getConnection(url,userName,password);
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            System.out.println("Ket noi loi");
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+    public static Connection getConnection()
+    {
+        return GetConnectServer.Connectionn("LAPTOP-ATNHOQI8"," sa","12345");
+    }
+//    public static void main(String[] args) throws SQLException {
+//        Connection connection = GetConnectServer.getConnection();
+//        String sql = "SELECT * FROM Xe";
+//        PreparedStatement pstmt = connection.prepareStatement(sql);
+//        ResultSet rs = pstmt.executeQuery();
+//
+//        while( rs.next()) {
+//            System.out.println(rs.getString(1));
+//       }
+//    }
+}
